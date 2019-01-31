e6b017c3b46500744704b5bf552f73ea76dfb4cd
Chris Franklin <cf73021@gmail.com>
2019-01-31 17:02:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function createMultiplier(multiplierValue){
     return given * multiplierValue
   }
 }
-var doubler = createMultiplier(2)
-var tripler = createMultiplier(3)
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
